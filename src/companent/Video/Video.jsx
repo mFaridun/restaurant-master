961f08c22808105c70ffce0da59b7b2bd8683538
mFaridun <musirmonovfaridun@gmail.com>
2023-04-26 23:59:42 +0500

    new commit

---
+++ src/companent/Video/Video.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import images from '../../constants/images';
+import icon from '../../constants/icon';
+import './Video.css';
+
+function Video () {
+
+  return (
+    <div>
+      <div className='video-sectiom'>
+        <div className='video-box'>
+          <img src="" alt="" />
+        </div>
+      </div>
+    </div>
+  )
+}
