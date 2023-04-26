ee2d25b763ad69e3502f547c17703826d18f9742
mFaridun <musirmonovfaridun@gmail.com>
2023-04-26 23:54:29 +0500

    new section

--- src/companent/News/News.jsx
+++ src/companent/News/News.jsx
@@ -7,6 +7,64 @@ function News () {
   return (
     <div>
 
+      <div className='news-section'>
+        <div className='container'>
+
+          <div className='news-box'>
+            <h1>News</h1>
+            <span>Gerícht updates</span>
+
+            <div className='news-card-box'>
+            <div className='news-card'>
+              <img src={images.gosht} alt="gosht" width={410} height={352} />
+
+              <div className='news-card-title'>
+                <span>tips for prepping and caring for your grill</span>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Volutpat mattis ipsum turpis elit elit scelerisque egestas mus in.</p>
+              <div className='news-data'>
+                <span>Read More</span>
+                <p>16 Apr 2021</p>
+              </div>
+              </div>
+
+            </div>
+
+            <div className='news-card'>
+              <img src={images.gosht} alt="gosht" width={410} height={352} />
+
+              <div className='news-card-title'>
+                <span>tips for prepping and caring for your grill</span>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Volutpat mattis ipsum turpis elit elit scelerisque egestas mus in.</p>
+              <div className='news-data'>
+                <span>Read More</span>
+                <p>16 Apr 2021</p>
+              </div>
+              </div>
+
+            </div>
+
+            <div className='news-card'>
+              <img src={images.gosht} alt="gosht" width={410} height={352} />
+
+              <div className='news-card-title'>
+                <span>tips for prepping and caring for your grill</span>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Volutpat mattis ipsum turpis elit elit scelerisque egestas mus in.</p>
+              <div className='news-data'>
+                <span>Read More</span>
+                <p>16 Apr 2021</p>
+              </div>
+              </div>
+
+            </div>
+            </div>
+
+            <div className='news-link'><a href="">View More</a></div>
+
+          </div>
+
+        </div>
+      </div>
+
     </div>
   )
 }
--- src/constants/images.js
+++ src/constants/images.js
@@ -1,8 +1,9 @@
 import logo from '../assets/logo-nav.png';
 import cabob from '../assets/cabob.svg';
 import backCircle from '../assets/back-circle.png';
-import klipartz from '../assets/klipartz.jpg'
-import stars from '../assets/stars.png'
+import klipartz from '../assets/klipartz.jpg';
+import stars from '../assets/stars.png';
+import gosht from '../assets/gosht.jpg';
 
 export default {
 
@@ -11,4 +12,5 @@ export default {
   backCircle,
   klipartz,
   stars,
+  gosht,
 };
